9f3362d7752f182a9d067c79b794b0426c306dad
Jinseob Kim <jinseob2kim@gmail.com>
2019-05-11 12:43:08 +0900

    s

--- R/jsBasicGadget.R
+++ R/jsBasicGadget.R
@@ -21,8 +21,8 @@
 #' @import shiny
 
 jsBasicGadget <- function(data, nfactor.limit = 20) {
-  requireNamespace("survival")
-  requireNamespace("survC1")
+  library("survival")
+  library("survC1")
 
   change.vnlist = list(c(" ", "_"), c("=<", "_le_"), c("=>", "_ge_"), c("=", "_eq_"), c("\\(", "_open_"), c("\\)", "_close_"), c("%", "_percent_"), c("-", "_"), c("/", "_"),
                        c("\r\n", "_"), c(",", "_comma_"))
