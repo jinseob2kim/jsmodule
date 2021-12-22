2d64240d7e3727c26941a075885e953d54b9309b
Hyunki <leevenstar@naver.com>
2021-12-22 11:46:12 +0000

    test

--- R/box.R
+++ R/box.R
@@ -315,6 +315,40 @@ boxServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limit
 
 
 #
+# ui <- navbarPage("basic statistics",
+#                  navbarMenu("Plot", icon = icon("bar-chart-o"),
+#                             tabPanel("Boxplot",
+#                                      sidebarLayout(
+#                                        sidebarPanel(
+#                                          boxUI("box")
+#                                        ),
+#                                        mainPanel(
+#                                          withLoader(plotOutput("box_plot"), type="html", loader="loader6"),
+#                                          ggplotdownUI("box")
+#                                        )
+#                                      )
+#                             )
+#                  )
+# )
+#
+# server <- function(input, output, session){
+#
+#       data <- reactive(mtcars)
+#       data.label <- reactive(jstable::mk.lev(mtcars))
+#       out_box <- boxServer("box", data = data, data_label = data.label, data_varStruct = NULL, nfactor.limit = 20)
+#
+#       output$box_plot <- renderPlot({
+#         print(out_box())
+#
+#
+ # output$box_plot <- renderPlot({
+  #  print(out_box())
+  })
+}
+
+shinyApp(ui, server)
+
+#
 #
 # ui <- fluidPage(
 #   sidebarLayout(
