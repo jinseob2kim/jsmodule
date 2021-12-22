1a2229c4623d45910a2af095689b40a70b9d77a2
Hyunki <leevenstar@naver.com>
2021-12-22 09:03:02 +0000

    test

--- R/bar.R
+++ R/bar.R
@@ -38,7 +38,7 @@
 barUI <- function(id, label = "barplot") {
   # Create a namespace function using the provided id
   ns <- NS(id)
-  
+
   tagList(
     uiOutput(ns("vars_bar")),
     uiOutput(ns("strata_bar")),
@@ -47,10 +47,54 @@ barUI <- function(id, label = "barplot") {
     checkboxInput(ns("jitter"), "Jitter"),
     uiOutput(ns("subvar")),
     uiOutput(ns("subval"))
-    
+
   )
 }
 
+
+#' @title barServer: shiny module server for barplot.
+#' @description Shiny module server for barplot.
+#' @param id id
+#' @param data Reactive data
+#' @param data_label Reactive data label
+#' @param data_varStruct Reactive List of variable structure, Default: NULL
+#' @param nfactor.limit nlevels limit in factor variable, Default: 10
+#' @return Shiny module server for barplot.
+#' @details Shiny module server for barplot.
+#' @examples
+#' library(shiny);library(ggplot2);library(ggpubr);
+#' ui <- fluidPage(
+#'    sidebarLayout(
+#'    sidebarPanel(
+#'      barUI("bar")
+#'    ),
+#'    mainPanel(
+#'      plotOutput("bar_plot"),
+#'      ggplotdownUI("bar")
+#'    )
+#'  )
+#')
+#'
+#' server <- function(input, output, session) {
+#'
+#'   data <- reactive(mtcars)
+#'   data.label <- reactive(jstable::mk.lev(mtcars))
+#'
+#'   out_bar <- barServer("bar", data = data, data_label = data.label,
+#'     data_varStruct = NULL)
+#'
+#'   output$bar_plot <- renderPlot({
+#'     print(out_bar())
+#'   })
+#'}
+#' @rdname barServer
+#' @export
+#' @import shiny
+#' @importFrom data.table data.table .SD :=
+#' @importFrom ggpubr ggbar
+#' @importFrom ggplot2 ggsave
+#' @importFrom rvg dml
+#' @importFrom officer read_pptx add_slide ph_with ph_location
 
 
 
@@ -59,34 +103,34 @@ barServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limit
                function(input, output, session) {
                  ## To remove NOTE.
                  level <- val_label <- variable <- NULL
-                 
+
                  if (is.null(data_varStruct)){
                    data_varStruct = reactive(list(variable = names(data())))
                  }
-                 
-                 
+
+
                  vlist <- reactive({
-                   
+
                    data <- data.table(data(), stringsAsFactors = T)
-                   
+
                    factor_vars <- names(data)[data[, lapply(.SD, class) %in% c("factor", "character")]]
                    #data[, (factor_vars) := lapply(.SD, as.factor), .SDcols= factor_vars]
                    factor_list <- mklist(data_varStruct(), factor_vars)
-                   
+
                    nclass_factor <- unlist(data[, lapply(.SD, function(x){length(levels(x))}), .SDcols = factor_vars])
-                   
+
                    group_vars <- factor_vars[nclass_factor >=2 & nclass_factor <= nfactor.limit & nclass_factor < nrow(data)]
                    group_list <- mklist(data_varStruct(), group_vars)
-                   
+
                    except_vars <- factor_vars[nclass_factor > nfactor.limit | nclass_factor == 1 | nclass_factor == nrow(data)]
-                   
+
                    select_vars <- setdiff(names(data), factor_vars)
                    select_list <- mklist(data_varStruct(), select_vars)
-                   
+
                    return(list(factor_vars = factor_vars, factor_list = factor_list, nclass_factor = nclass_factor, group_vars = group_vars, group_list = group_list, except_vars = except_vars,
                                select_vars = select_vars, select_list = select_list))
                  })
-                 
+
                  output$vars_bar <- renderUI({
                    tagList(
                      selectizeInput(session$ns("x_bar"), "X variable",
@@ -98,9 +142,9 @@ barServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limit
                                     selected = ifelse(length(vlist()$select_vars) > 1, vlist()$select_vars[2], vlist()$select_vars[1])
                      )
                    )
-                   
+
                  })
-                 
+
                  output$strata_bar <- renderUI({
                    strata_vars <- setdiff(vlist()$factor_vars, vlist()$except_vars)
                    strata_vars <- setdiff(strata_vars, input$x_bar)
@@ -110,40 +154,40 @@ barServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limit
                                   choices = strata_select, multiple = F,
                                   selected = unlist(strata_select)[1]
                    )
-                   
+
                  })
-                 
-                 
+
+
                  observeEvent(input$subcheck, {
                    output$subvar <- renderUI({
                      req(input$subcheck == T)
                      req(!is.null(input$x_bar))
-                     
+
                      var_subgroup <- setdiff(names(data()), c(vlist()$except_vars, input$x_bar, input$y_bar, input$strata))
-                     
+
                      var_subgroup_list <- mklist(data_varStruct(), var_subgroup)
                      validate(
                        need(length(var_subgroup) > 0 , "No variables for sub-group analysis")
                      )
-                     
+
                      tagList(
                        selectInput(session$ns("subvar_km"), "Sub-group variables",
                                    choices = var_subgroup_list, multiple = T,
                                    selected = var_subgroup[1])
                      )
-                     
-                     
+
+
                    })
-                   
+
                  })
-                 
-                 
+
+
                  output$subval <- renderUI({
                    req(input$subcheck == T)
                    req(length(input$subvar_km) > 0)
-                   
+
                    outUI <- tagList()
-                   
+
                    for (v in seq_along(input$subvar_km)){
                      if (input$subvar_km[[v]] %in% vlist()$factor_vars){
                        outUI[[v]] <- selectInput(session$ns(paste0("subval_km", v)), paste0("Sub-group value: ", input$subvar_km[[v]]),
@@ -155,12 +199,12 @@ barServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limit
                                                  min = val[1], max = val[5],
                                                  value = c(val[2], val[4]))
                      }
-                     
+
                    }
                    outUI
-                   
+
                  })
-                 
+
                  barInput <- reactive({
                    req(c(input$x_bar, input$y_bar, input$strata))
                    data <- data.table(data())
@@ -170,7 +214,7 @@ barServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limit
                    if(input$fill){
                      fill <- ifelse(input$strata == "None", input$x_bar, input$strata)
                    }
-                   
+
                    if (input$strata != "None"){
                      data <- data[!is.na(get(input$strata))]
                    }
@@ -187,17 +231,17 @@ barServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limit
                    if (input$mean & input$jitter){
                      add <- c("jitter", "mean_se")
                    }
-                   
-            
-                   
+
+
+
                    ggpubr::ggbarplot(data, input$x_bar, input$y_bar, color = color, add = add, add.params = add.params, conf.int = input$lineci,
                                      xlab = label[variable == input$x_bar, var_label][1],
-                                     ylab = label[variable == input$y_bar, var_label][1], na.rm = T, 
-                                     position = position_dodge(),fill=fill, 
+                                     ylab = label[variable == input$y_bar, var_label][1], na.rm = T,
+                                     position = position_dodge(),fill=fill,
 
                    )
                  })
-                 
+
                  output$downloadControls <- renderUI({
                    tagList(
                      column(4,
@@ -218,11 +262,11 @@ barServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limit
                      )
                    )
                  })
-                 
+
                  output$downloadButton <- downloadHandler(
                    filename =  function() {
                      paste(input$x_bar, "_", input$y_bar,"_barplot.",input$file_ext ,sep="")
-                     
+
                    },
                    # content is a function with argument file. content writes the plot to the device
                    content = function(file) {
@@ -232,55 +276,55 @@ barServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limit
                                       incProgress(1/15)
                                       Sys.sleep(0.01)
                                     }
-                                    
+
                                     if (input$file_ext == "pptx"){
                                       my_vec_graph <- rvg::dml(ggobj  = barInput())
-                                      
+
                                       doc <- officer::read_pptx()
                                       doc <- officer::add_slide(doc, layout = "Title and Content", master = "Office Theme")
                                       doc <- officer::ph_with(doc, my_vec_graph, location = officer::ph_location(width = input$fig_width, height = input$fig_height))
                                       print(doc, target = file)
-                                      
+
                                     } else{
                                       ggplot2::ggsave(file, barInput(), dpi = 300, units = "in", width = input$fig_width, height =input$fig_height)
                                     }
                                   })
-                     
+
                    }
                  )
-                 
+
                  return(barInput)
-                 
-                 
-                 
-                 
-                 
+
+
+
+
+
                }
   )
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 #####
 
 
-# 
-# 
+#
+#
 # ui <- fluidPage(
 #   sidebarLayout(
 #     sidebarPanel(
@@ -292,7 +336,7 @@ barServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limit
 #     )
 #   )
 # )
-# 
+#
 # server <- function(input, output, session) {
 #   mtcars$am <- as.factor(mtcars$am)
 #   mtcars$vs <- as.factor(mtcars$vs)
@@ -303,10 +347,10 @@ barServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limit
 #   data.label <- reactive(jstable::mk.lev(mtcars))
 #   out_bar <- barServer("bar", data = data, data_label = data.label,
 #                            data_varStruct = NULL)
-#   
+#
 #   output$bar_plot <- renderPlot({
 #     print(out_bar())
 #   })
 # }
-# 
+#
 # shinyApp(ui, server)
--- R/line.R
+++ R/line.R
@@ -37,7 +37,7 @@
 lineUI <- function(id, label = "lineplot") {
   # Create a namespace function using the provided id
   ns <- NS(id)
-  
+
   tagList(
     uiOutput(ns("vars_line")),
     uiOutput(ns("strata_line")),
@@ -46,10 +46,54 @@ lineUI <- function(id, label = "lineplot") {
     checkboxInput(ns("jitter"), "Jitter"),
     uiOutput(ns("subvar")),
     uiOutput(ns("subval"))
-    
+
   )
 }
 
+
+#' @title lineServer: shiny module server for lineplot.
+#' @description Shiny module server for lineplot.
+#' @param id id
+#' @param data Reactive data
+#' @param data_label Reactive data label
+#' @param data_varStruct Reactive List of variable structure, Default: NULL
+#' @param nfactor.limit nlevels limit in factor variable, Default: 10
+#' @return Shiny module server for lineplot.
+#' @details Shiny module server for lineplot.
+#' @examples
+#' library(shiny);library(ggplot2);library(ggpubr);
+#' ui <- fluidPage(
+#'    sidebarLayout(
+#'    sidebarPanel(
+#'      lineUI("line")
+#'    ),
+#'    mainPanel(
+#'      plotOutput("line_plot"),
+#'      ggplotdownUI("line")
+#'    )
+#'  )
+#')
+#'
+#' server <- function(input, output, session) {
+#'
+#'   data <- reactive(mtcars)
+#'   data.label <- reactive(jstable::mk.lev(mtcars))
+#'
+#'   out_line <- lineServer("line", data = data, data_label = data.label,
+#'     data_varStruct = NULL)
+#'
+#'   output$line_plot <- renderPlot({
+#'     print(out_line())
+#'   })
+#'}
+#' @rdname lineServer
+#' @export
+#' @import shiny
+#' @importFrom data.table data.table .SD :=
+#' @importFrom ggpubr ggline
+#' @importFrom ggplot2 ggsave
+#' @importFrom rvg dml
+#' @importFrom officer read_pptx add_slide ph_with ph_location
 
 
 
@@ -58,34 +102,34 @@ lineServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limi
                function(input, output, session) {
                  ## To remove NOTE.
                  level <- val_label <- variable <- NULL
-                 
+
                  if (is.null(data_varStruct)){
                    data_varStruct = reactive(list(variable = names(data())))
                  }
-                 
-                 
+
+
                  vlist <- reactive({
-                   
+
                    data <- data.table(data(), stringsAsFactors = T)
-                   
+
                    factor_vars <- names(data)[data[, lapply(.SD, class) %in% c("factor", "character")]]
                    #data[, (factor_vars) := lapply(.SD, as.factor), .SDcols= factor_vars]
                    factor_list <- mklist(data_varStruct(), factor_vars)
-                   
+
                    nclass_factor <- unlist(data[, lapply(.SD, function(x){length(levels(x))}), .SDcols = factor_vars])
-                   
+
                    group_vars <- factor_vars[nclass_factor >=2 & nclass_factor <= nfactor.limit & nclass_factor < nrow(data)]
                    group_list <- mklist(data_varStruct(), group_vars)
-                   
+
                    except_vars <- factor_vars[nclass_factor > nfactor.limit | nclass_factor == 1 | nclass_factor == nrow(data)]
-                   
+
                    select_vars <- setdiff(names(data), factor_vars)
                    select_list <- mklist(data_varStruct(), select_vars)
-                   
+
                    return(list(factor_vars = factor_vars, factor_list = factor_list, nclass_factor = nclass_factor, group_vars = group_vars, group_list = group_list, except_vars = except_vars,
                                select_vars = select_vars, select_list = select_list))
                  })
-                 
+
                  output$vars_line <- renderUI({
                    tagList(
                      selectizeInput(session$ns("x_line"), "X variable",
@@ -97,9 +141,9 @@ lineServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limi
                                     selected = ifelse(length(vlist()$select_vars) > 1, vlist()$select_vars[2], vlist()$select_vars[1])
                      )
                    )
-                   
+
                  })
-                 
+
                  output$strata_line <- renderUI({
                    strata_vars <- setdiff(vlist()$factor_vars, vlist()$except_vars)
                    strata_vars <- setdiff(strata_vars, input$x_line)
@@ -109,40 +153,40 @@ lineServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limi
                                   choices = strata_select, multiple = F,
                                   selected = unlist(strata_select)[1]
                    )
-                   
+
                  })
-                 
-                 
+
+
                  observeEvent(input$subcheck, {
                    output$subvar <- renderUI({
                      req(input$subcheck == T)
                      req(!is.null(input$x_line))
-                     
+
                      var_subgroup <- setdiff(names(data()), c(vlist()$except_vars, input$x_line, input$y_line, input$strata))
-                     
+
                      var_subgroup_list <- mklist(data_varStruct(), var_subgroup)
                      validate(
                        need(length(var_subgroup) > 0 , "No variables for sub-group analysis")
                      )
-                     
+
                      tagList(
                        selectInput(session$ns("subvar_km"), "Sub-group variables",
                                    choices = var_subgroup_list, multiple = T,
                                    selected = var_subgroup[1])
                      )
-                     
-                     
+
+
                    })
-                   
+
                  })
-                 
-                 
+
+
                  output$subval <- renderUI({
                    req(input$subcheck == T)
                    req(length(input$subvar_km) > 0)
-                   
+
                    outUI <- tagList()
-                   
+
                    for (v in seq_along(input$subvar_km)){
                      if (input$subvar_km[[v]] %in% vlist()$factor_vars){
                        outUI[[v]] <- selectInput(session$ns(paste0("subval_km", v)), paste0("Sub-group value: ", input$subvar_km[[v]]),
@@ -154,12 +198,12 @@ lineServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limi
                                                  min = val[1], max = val[5],
                                                  value = c(val[2], val[4]))
                      }
-                     
+
                    }
                    outUI
-                   
+
                  })
-                 
+
                  lineInput <- reactive({
                    req(c(input$x_line, input$y_line, input$strata))
                    data <- data.table(data())
@@ -174,10 +218,10 @@ lineServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limi
                                    "Mean_SE" = c("jitter", "mean_se"),
                                    "Mean_SD" = c("jitter","mean_sd"),
                                    "Median_IQR" = c("jitter", "median_iqr")
-                     ) 
+                     )
                    }
-                   
-                   
+
+
                    color <- ifelse(input$strata == "None", "black", input$strata)
                    fill <- ifelse(input$strata=="None", input$x_line , input$strata)
                    if (input$strata != "None"){
@@ -186,8 +230,8 @@ lineServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limi
                    add.params <- list()
                    cor.coeff.args <- list(p.accuracy = 0.001)
 
-                   
-                   
+
+
                    linetype = 19
                    if (input$linetype){
                      if(input$strata == "None"){
@@ -195,17 +239,17 @@ lineServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limi
                      } else {
                        linetype <- input$strata
                      }
-                     
+
                    }
-                   
-                   
+
+
                    ggpubr::ggline(data, input$x_line, input$y_line, color = color, add = add, add.params = add.params, conf.int = input$lineci,
                                   xlab = label[variable == input$x_line, var_label][1],
-                                  ylab = label[variable == input$y_line, var_label][1], na.rm = T, 
+                                  ylab = label[variable == input$y_line, var_label][1], na.rm = T,
                                   linetype = linetype
                    )
                  })
-                 
+
                  output$downloadControls <- renderUI({
                    tagList(
                      column(4,
@@ -226,11 +270,11 @@ lineServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limi
                      )
                    )
                  })
-                 
+
                  output$downloadButton <- downloadHandler(
                    filename =  function() {
                      paste(input$x_line, "_", input$y_line,"_lineplot.",input$file_ext ,sep="")
-                     
+
                    },
                    # content is a function with argument file. content writes the plot to the device
                    content = function(file) {
@@ -240,49 +284,49 @@ lineServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limi
                                       incProgress(1/15)
                                       Sys.sleep(0.01)
                                     }
-                                    
+
                                     if (input$file_ext == "pptx"){
                                       my_vec_graph <- rvg::dml(ggobj  = lineInput())
-                                      
+
                                       doc <- officer::read_pptx()
                                       doc <- officer::add_slide(doc, layout = "Title and Content", master = "Office Theme")
                                       doc <- officer::ph_with(doc, my_vec_graph, location = officer::ph_location(width = input$fig_width, height = input$fig_height))
                                       print(doc, target = file)
-                                      
+
                                     } else{
                                       ggplot2::ggsave(file, lineInput(), dpi = 300, units = "in", width = input$fig_width, height =input$fig_height)
                                     }
                                   })
-                     
+
                    }
                  )
-                 
+
                  return(lineInput)
-                 
-                 
-                 
-                 
-                 
+
+
+
+
+
                }
   )
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 #####
 
@@ -300,7 +344,7 @@ lineServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limi
 #     )
 #   )
 # )
-# 
+#
 # server <- function(input, output, session) {
 #   mtcars$am <- as.factor(mtcars$am)
 #   mtcars$vs <- as.factor(mtcars$vs)
@@ -311,10 +355,10 @@ lineServer <- function(id, data, data_label, data_varStruct = NULL, nfactor.limi
 #   data.label <- reactive(jstable::mk.lev(mtcars))
 #   out_line <- lineServer("line", data = data, data_label = data.label,
 #                              data_varStruct = NULL)
-#   
+#
 #   output$line_plot <- renderPlot({
 #     print(out_line())
 #   })
 # }
-# 
+#
 # shinyApp(ui, server)
